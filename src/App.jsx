c02186a314ec21c0536380b4bae6b8e26dbcfc79
ameramhmed <amera.elshafie.internship.2026@gmail.com>
2026-05-13 14:56:03 +0300

    feat: add protected routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import Dashboard from "./pages/Dashboard";
 import AddTask from "./pages/AddTask";
+import ProtectedRoute from "./ProtectedRoute";
 
 function App() {
   return (
@@ -10,8 +11,16 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/add-task" element={<AddTask />} />
+        <Route path="/dashboard" element={
+          <ProtectedRoute>
+            <Dashboard />
+          </ProtectedRoute>
+        } />
+        <Route path="/add-task" element={
+          <ProtectedRoute>
+            <AddTask />
+          </ProtectedRoute>
+        } />
       </Routes>
     </BrowserRouter>
   );
